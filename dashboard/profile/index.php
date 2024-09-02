b69be850a3d76b1d1b4c7a71723b1952f675bcfc
Mayer Bálint <center@gymoneglobal.com>
2024-09-02 20:41:54 +0200

    Pre-Beta 0.2.5.4
    Lang File's (+)
    Some Bugfix (V)
    Profile password changer Functions (+)

--- dashboard/profile/index.php
+++ dashboard/profile/index.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+require_once '../../vendor/autoload.php'; // COMPOSER!
+
+
 if (!isset($_SESSION['userid'])) {
     header("Location: ../");
     exit();
@@ -36,6 +39,11 @@ $db_name = $env_data['DB_NAME'] ?? '';
 $business_name = $env_data['BUSINESS_NAME'] ?? '';
 $lang_code = $env_data['LANG_CODE'] ?? '';
 $version = $env_data["APP_VERSION"] ?? '';
+$smtp_host = $env_data['MAIL_HOST'] ?? '';
+$smtp_port = $env_data['MAIL_PORT'] ?? '';
+$smtp_encryption = $env_data['MAIL_ENCRYPTION'] ?? '';
+$smtp_username = $env_data['MAIL_USERNAME'] ?? '';
+$smtp_password = $env_data['MAIL_PASSWORD'] ?? '';
 
 $lang = $lang_code;
 
@@ -55,14 +63,311 @@ if ($conn->connect_error) {
     die("Kapcsolódási hiba: " . $conn->connect_error);
 }
 
-$sql = "SELECT firstname, lastname FROM users WHERE userid = ?";
+$sql = "SELECT firstname, lastname, email FROM users WHERE userid = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $userid);
 $stmt->execute();
-$stmt->bind_result($lastname, $firstname);
+$stmt->bind_result($lastname, $firstname, $mail);
 $stmt->fetch();
 
 $stmt->close();
+$alerts_html = '';
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$host = $_SERVER['HTTP_HOST'];
+$domain_url = $protocol . $host;
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['currentPassword'], $_POST['newPassword'], $_POST['confirmPassword'])) {
+        $userid = $_SESSION['userid'];
+        $currentPassword = $_POST['currentPassword'];
+        $newPassword = $_POST['newPassword'];
+        $confirmPassword = $_POST['confirmPassword'];
+
+        if ($newPassword !== $confirmPassword) {
+            $alerts_html .= '<div class="alert alert-warning" role="alert">
+                            ' . $translations["twopasswordnot"] . '
+                        </div>';
+            header("Refresh:2");
+        } else {
+            $sql = "SELECT password FROM users WHERE userid = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("i", $userid);
+            $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($hashedPassword);
+            $stmt->fetch();
+
+            if (!password_verify($currentPassword, $hashedPassword)) {
+                $alerts_html .= '<div class="alert alert-danger" role="alert">
+                                    ' . $translations["error-old-password"] . '
+                                </div>';
+                header("Refresh:2");
+            } else {
+                $newHashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+                $updateSql = "UPDATE users SET password = ? WHERE userid = ?";
+                $updateStmt = $conn->prepare($updateSql);
+                $updateStmt->bind_param("si", $newHashedPassword, $userid);
+
+                if ($updateStmt->execute()) {
+                    $alerts_html .= '<div class="alert alert-success" role="alert">
+                                    ' . $translations["success-new-password"] . '
+                                </div>';
+                    $transport = (new Swift_SmtpTransport($smtp_host, $smtp_port, $smtp_encryption))
+                        ->setUsername($smtp_username)
+                        ->setPassword($smtp_password);
+
+                    $mailer = new Swift_Mailer($transport);
+
+                    $editedcontent = <<<EOD
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+  <!--[if !mso]><!-->
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <!--<![endif]-->
+  <!--[if (gte mso 9)|(IE)]>
+  <xml>
+    <o:OfficeDocumentSettings>
+      <o:AllowPNG/>
+      <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+  </xml>
+  <![endif]-->
+  <!--[if (gte mso 9)|(IE)]>
+<style type="text/css">
+body {width: 600px;margin: 0 auto;}
+table {border-collapse: collapse;}
+table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
+img {-ms-interpolation-mode: bicubic;}
+</style>
+<![endif]-->
+  <style type="text/css">
+body, p, div {
+  font-family: arial,helvetica,sans-serif;
+  font-size: 14px;
+}
+body {
+  color: #000000;
+}
+body a {
+  color: #1188E6;
+  text-decoration: none;
+}
+p { margin: 0; padding: 0; }
+table.wrapper {
+  width:100% !important;
+  table-layout: fixed;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+}
+img.max-width {
+  max-width: 100% !important;
+}
+.column.of-2 {
+  width: 50%;
+}
+.column.of-3 {
+  width: 33.333%;
+}
+.column.of-4 {
+  width: 25%;
+}
+ul ul ul ul  {
+  list-style-type: disc !important;
+}
+ol ol {
+  list-style-type: lower-roman !important;
+}
+ol ol ol {
+  list-style-type: lower-latin !important;
+}
+ol ol ol ol {
+  list-style-type: decimal !important;
+}
+@media screen and (max-width:480px) {
+  .preheader .rightColumnContent,
+  .footer .rightColumnContent {
+    text-align: left !important;
+  }
+  .preheader .rightColumnContent div,
+  .preheader .rightColumnContent span,
+  .footer .rightColumnContent div,
+  .footer .rightColumnContent span {
+    text-align: left !important;
+  }
+  .preheader .rightColumnContent,
+  .preheader .leftColumnContent {
+    font-size: 80% !important;
+    padding: 5px 0;
+  }
+  table.wrapper-mobile {
+    width: 100% !important;
+    table-layout: fixed;
+  }
+  img.max-width {
+    height: auto !important;
+    max-width: 100% !important;
+  }
+  a.bulletproof-button {
+    display: block !important;
+    width: auto !important;
+    font-size: 80%;
+    padding-left: 0 !important;
+    padding-right: 0 !important;
+  }
+  .columns {
+    width: 100% !important;
+  }
+  .column {
+    display: block !important;
+    width: 100% !important;
+    padding-left: 0 !important;
+    padding-right: 0 !important;
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+  }
+  .social-icon-column {
+    display: inline-block !important;
+  }
+}
+</style>
+  <!--user entered Head Start--><!--End Head user entered-->
+</head>
+<body>
+  <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size:14px; font-family:arial,helvetica,sans-serif; color:#000000; background-color:#FFFFFF;">
+    <div class="webkit">
+      <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#FFFFFF">
+        <tr>
+          <td valign="top" bgcolor="#FFFFFF" width="100%">
+            <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                <td width="100%">
+                  <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                    <tr>
+                      <td>
+                        <!--[if mso]>
+<center>
+<table><tr><td width="600">
+<![endif]-->
+                                <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
+                                  <tr>
+                                    <td role="modules-container" style="padding:0px 0px 0px 0px; color:#000000; text-align:left;" bgcolor="#FFFFFF" width="100%" align="left"><table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+<tr>
+  <td role="module-content">
+    <p></p>
+  </td>
+</tr>
+</table><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" role="module" data-type="columns" style="padding:0px 0px 0px 0px;" bgcolor="#FFFFFF" data-distribution="1">
+<tbody>
+  <tr role="module-content">
+    <td height="100%" valign="top"><table width="580" style="width:580px; border-spacing:0; border-collapse:collapse; margin:0px 10px 0px 10px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="" class="column column-0">
+  <tbody>
+    <tr>
+      <td style="padding:0px;margin:0px;border-spacing:0;"><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="dae5891b-ceee-40f7-9315-02ea0b72592e">
+<tbody>
+  <tr>
+    <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+      <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:20% !important; width:20%; height:auto !important;" width="116" alt="" data-proportionally-constrained="true" data-responsive="true" src="{$domain_url}/assets/img/brand/logo.png">
+    </td>
+  </tr>
+</tbody>
+</table></td>
+    </tr>
+  </tbody>
+</table></td>
+  </tr>
+</tbody>
+</table><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" role="module" data-type="columns" style="padding:10px 0px 10px 0px;" bgcolor="#FFFFFF" data-distribution="1">
+<tbody>
+  <tr role="module-content">
+    <td height="100%" valign="top"><table width="580" style="width:580px; border-spacing:0; border-collapse:collapse; margin:0px 10px 0px 10px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="" class="column column-0">
+  <tbody>
+    <tr>
+      <td style="padding:0px;margin:0px;border-spacing:0;"><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="c09ad11e-b6f0-426b-bfb5-e854fb1d6b4e">
+<tbody>
+  <tr>
+    <td style="padding:18px 0px 18px 0px; line-height:30px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><h2 style="text-align: center">{$business_name}</h2>
+<div style="font-family: inherit; text-align: center">{$translations["editedpasswordbody"]}</div><div></div></div></td>
+  </tr>
+</tbody>
+</table><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" role="module" data-type="columns" style="padding:0px 0px 0px 0px;" bgcolor="#252525" data-distribution="1">
+<tbody>
+  <tr role="module-content">
+    <td height="100%" valign="top"><table width="580" style="width:580px; border-spacing:0; border-collapse:collapse; margin:0px 10px 0px 10px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="" class="column column-0">
+  <tbody>
+    <tr>
+      <td style="padding:0px;margin:0px;border-spacing:0;"><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="70667641-28f8-4e30-850f-c1783cac6e0b">
+<tbody>
+  <tr>
+    <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+      <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:10% !important; width:10%; height:auto !important;" width="58" alt="" data-proportionally-constrained="true" data-responsive="true" src="https://gymoneglobal.com/assets/img/text-color-logo.png">
+    </td>
+  </tr>
+</tbody>
+</table></td>
+    </tr>
+  </tbody>
+</table></td>
+  </tr>
+</tbody>
+</table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="background-color:#252525; color:#444444; font-size:12px; line-height:20px; padding:0px 0px 0px 0px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"></div><p style="font-size:12px; line-height:20px;"><a class="Unsubscribe--unsubscribeLink" href="https://gymoneglobal.com/" target="_blank" style="">Gymoneglobal.com</a></p></div></td>
+                                  </tr>
+                                </table>
+                                <!--[if mso]>
+                              </td>
+                            </tr>
+                          </table>
+                        </center>
+                        <![endif]-->
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </center>
+</body>
+</html>
+EOD;
+
+                    $message = (new Swift_Message($translations["passwordedited"]))
+                        ->setFrom(["{$smtp_username}" => "{$business_name} - {$translations['passwordedited']}"])
+                        ->setTo([$mail => '{$firstname}'])
+                        ->setBody($editedcontent, 'text/html');
+
+                    $result = $mailer->send($message);
+                    header("Refresh:2");
+                } else {
+                    $alerts_html .= '<div class="alert alert-danger" role="alert">
+                                    ' . $translations["error-old-password"] . '
+                                </div>';
+                    header("Refresh:2");
+                }
+
+                $updateStmt->close();
+            }
+
+            $stmt->close();
+        }
+    } else {
+        $alerts_html .= '<div class="alert alert-danger" role="alert">
+                        ' . $translations["unexpected-error"] . '
+                    </div>';
+        header("Refresh:2");
+    }
+}
+
+
 $conn->close();
 ?>
 
@@ -72,7 +377,7 @@ $conn->close();
 
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $business_name; ?> <?php echo $translations["dashboard"]; ?></title>
+    <title><?php echo $business_name; ?> - <?php echo $translations["dashboard"]; ?></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -133,6 +438,7 @@ $conn->close();
                 <div class="row">
 
                 </div>
+                <?php echo $alerts_html; ?>
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="card">
@@ -194,31 +500,32 @@ $conn->close();
                     <div class="col-sm-3">
                         <div class="card">
                             <div class="card-body">
-                                <form id="passwordChangeForm">
+                                <form id="passwordChangeForm" method="POST" action="">
                                     <div class="row">
                                         <div class="col-md-12">
                                             <div class="mb-3">
                                                 <div class="form-group">
                                                     <label for="currentPassword" class="form-label">Eredeti jelszó:</label>
-                                                    <input type="password" class="form-control" id="currentPassword" required>
+                                                    <input type="password" class="form-control" id="currentPassword" name="currentPassword" required>
                                                 </div>
                                             </div>
                                             <div class="mb-3">
                                                 <div class="form-group">
                                                     <label for="newPassword" class="form-label">Új jelszó:</label>
-                                                    <input type="password" class="form-control" id="newPassword" required>
+                                                    <input type="password" class="form-control" id="newPassword" name="newPassword" required>
                                                 </div>
                                             </div>
                                             <div class="mb-3">
                                                 <div class="form-group">
                                                     <label for="confirmPassword" class="form-label">Új jelszó megerősítése:</label>
-                                                    <input type="password" class="form-control" id="confirmPassword" required>
+                                                    <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <button type="submit" class="btn btn-primary">Jelszó módosítása</button>
                                 </form>
+
                             </div>
                         </div>
                     </div>
--- register/index.php
+++ register/index.php
@@ -262,7 +262,7 @@ ol ol ol ol {
 <tbody>
   <tr>
     <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
-      <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:20% !important; width:20%; height:auto !important;" width="116" alt="" data-proportionally-constrained="true" data-responsive="true" src="https://gymoneglobal.com/assets/img/text-color-logo.png">
+      <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:20% !important; width:20%; height:auto !important;" width="116" alt="" data-proportionally-constrained="true" data-responsive="true" src="{$domain_url}/assets/img/brand/logo.png">
     </td>
   </tr>
 </tbody>
